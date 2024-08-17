399e1823350a916a81d1479824fb58cb95301a03
nouaridev <smailnobiti@gmail.com>
2024-08-17 23:18:29 +0100

    m

--- main.js
+++ main.js
@@ -16,3 +16,56 @@
 // activities
 
 
+
+
+// administration
+function openModal() {
+    document.getElementById('editModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('editModal').style.display = 'none';
+}
+
+function openModalAdd(){
+    document.getElementById('AddModal').style.display = 'block';
+}
+
+function closeModalAdd() {
+    document.getElementById('AddModal').style.display = 'none';
+}
+function deleteModal() {
+    document.getElementById('deleteModal').style.display = 'block';
+}
+function closeModaldelete() {
+    document.getElementById('deleteModal').style.display = 'none';
+}
+
+// administration
+
+
+
+//quiz&questions
+    function closeAddQuiz(){
+        document.querySelector('.add-quiz').style.display = 'none';
+    }
+    function openAddQuiz(){
+        document.querySelector('.add-quiz').style.display= 'block';
+    }
+
+    function closeAddCat(){
+        document.querySelector('.add-cat').style.display = 'none';
+    }
+    function openAddCat(){
+        document.querySelector('.add-cat').style.display= 'block';
+    }
+
+    
+    function closeEditCat(){
+        document.querySelector('.edit-cat').style.display = 'none';
+    }
+    function openEditCat(){
+        document.querySelector('.edit-cat').style.display= 'block';
+    }
+//quiz&questions
+
